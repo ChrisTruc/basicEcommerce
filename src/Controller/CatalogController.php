776e9e31f7a82e5627501528eb9d9720fc49c683
ChrisTruc <christophe.truchet@yahoo.com>
2020-06-18 18:00:03 +0200

    add homepage product list

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CatalogController extends AbstractController
@@ -10,10 +14,15 @@ class CatalogController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index() : Response
     {
+        $products = $this->getDoctrine()
+                         ->getRepository(Product::class)
+                         ->findAll();
+        
+
         return $this->render('catalog/index.html.twig', [
-            'controller_name' => 'CatalogController',
+            'products' => $products,
         ]);
     }
 }
